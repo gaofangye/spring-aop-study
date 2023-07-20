6519aec2a9fea086e77a42263afa3de8aedbb53d
1033727528@qq.com <1033727528@qq.com>
2023-07-20 09:18:48 +0800

    init: Spring AOP 学习 Project 初始化

---
+++ src/main/java/cn/gaofangye/spring/aop/study/SpringAopStudyApplication.java
@@ -0,0 +1,13 @@
+package cn.gaofangye.spring.aop.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAopStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAopStudyApplication.class, args);
+    }
+
+}
